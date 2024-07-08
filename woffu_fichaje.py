34e486d4ffb361d602bc23bfb605bf5182ad67ae
Chals1992 <carles_casajuana@hotmail.com>
2024-07-08 13:30:38 +0200

    Update woffu_fichaje.py

--- woffu_fichaje.py
+++ woffu_fichaje.py
@@ -1,42 +1,21 @@
 import os
 import requests
-from msedge.selenium_tools import Edge, EdgeOptions
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
 
-# Función para descargar msedgedriver.exe desde la URL proporcionada
-def descargar_msedgedriver(url, archivo_destino):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            with open(archivo_destino, 'wb') as f:
-                f.write(response.content)
-            print(f"Archivo {archivo_destino} descargado correctamente.")
-        else:
-            print(f"Error al descargar el archivo. Código de estado: {response.status_code}")
-    except Exception as e:
-        print(f"Error al descargar el archivo: {str(e)}")
-
-# URL de descarga del archivo msedgedriver.exe
+# Descargar msedgedriver.exe
+archivo_msedgedriver = 'msedgedriver.exe'
 url_msedgedriver = "https://volkswagengroup-my.sharepoint.com/:u:/r/personal/carles_casajuana1_volkswagen-groupservices_com/Documents/Carles_Seat/edgedriver_win64/msedgedriver.exe?csf=1&web=1&e=qkd8R3"
-archivo_msedgedriver = "msedgedriver.exe"
+response = requests.get(url_msedgedriver)
+if response.status_code == 200:
+    with open(archivo_msedgedriver, 'wb') as file:
+        file.write(response.content)
+    print("msedgedriver.exe descargado con éxito.")
+else:
+    print(f"Error al descargar el archivo. Código de estado: {response.status_code}")
 
-# Descargar msedgedriver.exe si no existe en el directorio actual
-if not os.path.exists(archivo_msedgedriver):
-    print(f"Descargando {archivo_msedgedriver}...")
-    descargar_msedgedriver(url_msedgedriver, archivo_msedgedriver)
-
-# Configuración de opciones para el navegador Edge
-options = EdgeOptions()
-options.use_chromium = True
-
-# Inicialización del navegador Edge con la ubicación del ejecutable descargado
-driver = Edge(executable_path=os.path.abspath(archivo_msedgedriver), options=options)
-
-# Ejemplo de uso: abrir la URL de Woffu
-url_woffu = "https://www.woffu.com/"
-driver.get(url_woffu)
-
-# Aquí puedes continuar con las acciones que necesites automatizar en Woffu
-# Por ejemplo, llenar formularios, hacer clic en botones, etc.
-
-# Cuando hayas terminado, cierra el navegador
-driver.quit()
+options = Options()
+options.add_argument("headless")  # Ejecutar en modo headless, sin abrir el navegador
+service = Service(executable_path=os.path.abspath(archivo_msedgedriver))
+driver = webdriver.Edge(se
