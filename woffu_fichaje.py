c86dcb204c0d4d4bae625b4c66fc585ca121442e
Chals1992 <carles_casajuana@hotmail.com>
2024-07-08 10:36:58 +0200

    Update woffu_fichaje.py

--- woffu_fichaje.py
+++ woffu_fichaje.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # Configurar el WebDriver (en este caso, Chrome)
 options = Options()
@@ -15,13 +16,14 @@ driver.get("https://volkswagen-groupservices.woffu.com/v2/personal/dashboard/use
 # Maximiza la ventana (no es necesario si se está ejecutando en modo headless)
 # driver.maximize_window()
 
-# Espera a que el elemento se cargue antes de hacer clic
+# Espera a que el botón "Entrar" sea clickeable
 try:
-    # Esperar a que aparezca el botón "Entrar"
     element = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Entrar')]"))
     )
+    time.sleep(2)  # Espera adicional antes de hacer clic
     element.click()
+    print("Clic en el botón 'Entrar' exitoso.")
 except Exception as e:
     print(f"No se pudo hacer clic en el botón 'Entrar': {e}")
 
