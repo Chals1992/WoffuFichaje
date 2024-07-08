179fa7326d16c054a2a9007a1777a6c57bd06418
Chals1992 <carles_casajuana@hotmail.com>
2024-07-08 11:01:55 +0200

    Update woffu_fichaje.py

--- woffu_fichaje.py
+++ woffu_fichaje.py
@@ -1,37 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# Importar las clases necesarias desde msedge.selenium_tools
+from msedge.selenium_tools import Edge, EdgeOptions
 
-# Configura el WebDriver para Edge (Microsoft Edge)
-options = Options()
-options.add_argument("--headless")  # Ejecución en modo headless (sin ventana)
+# Crear las opciones para el navegador Edge
+options = EdgeOptions()
+options.use_chromium = True  # Utilizar el motor Chromium
+options.add_argument("start-maximized")  # Opción para maximizar la ventana del navegador
 
-# Ruta donde has descargado msedgedriver.exe
-driver_path = r'C:\Users\W1VFOUS\OneDrive - Volkswagen AG\Carles_Seat\edgedriver_win64\msedgedriver.exe'
+# Especificar la ruta al ejecutable de msedgedriver
+driver_path = 'C:\\Users\\W1VFOUS\\OneDrive - Volkswagen AG\\Carles_Seat\\edgedriver_win64\\msedgedriver.exe'
 
-# Inicializa el driver de Edge
-driver = webdriver.Edge(executable_path=driver_path, options=options)
+# Inicializar el WebDriver para Microsoft Edge
+driver = Edge(executable_path=driver_path, options=options)
 
-try:
-    # Abre la URL de Woffu
-    driver.get("https://volkswagen-groupservices.woffu.com")
-
-    # Maximiza la ventana (si es necesario)
-    driver.maximize_window()
-
-    # Espera a que el elemento 'Entrar' sea clickable y luego haz clic en él
-    element = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Entrar')]"))
-    )
-    element.click()
-    print("Clic en 'Entrar' realizado con éxito.")
-
-except Exception as e:
-    print(f"No se pudo hacer clic en 'Entrar': {e}")
-
-finally:
-    # Cierra el navegador al finalizar
-    driver.quit()
+# A partir de este punto, puedes utilizar 'driver' para interactuar con el navegador Edge
