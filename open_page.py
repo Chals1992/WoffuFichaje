6ab555a1ca846524ed4f26a2b684dd6c812bec63
Chals1992 <carles_casajuana@hotmail.com>
2024-07-11 14:10:53 +0200

    Update open_page.py

--- open_page.py
+++ open_page.py
@@ -10,7 +10,7 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-# Especifica la ruta al archivo chromedriver
+# Ruta al archivo chromedriver
 chromedriver_path = "/usr/local/bin/chromedriver"
 
 # Configurar el servicio de Chrome con la ruta del chromedriver
@@ -18,7 +18,7 @@ service = Service(executable_path=chromedriver_path)
 
 try:
     # Esperar unos segundos antes de crear el controlador de Chrome
-    time.sleep(10)
+    time.sleep(5)
     
     # Crear una instancia del controlador de Chrome
     driver = webdriver.Chrome(service=service, options=options)
@@ -31,3 +31,4 @@ try:
 
 except Exception as e:
     print(f"Error al ejecutar el WebDriver: {str(e)}")
+
