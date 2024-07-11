3ae5224cfcbacdcc08ab7ca915d1706e58cf3edc
Chals1992 <carles_casajuana@hotmail.com>
2024-07-11 16:13:28 +0200

    Update open_page.py

--- open_page.py
+++ open_page.py
@@ -6,7 +6,7 @@ options.add_argument("--headless")  # Ejecutar en modo headless
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 
-driver = Edge(executable_path='/usr/local/bin/edgedriver', options=options)
+driver = Edge(executable_path='/usr/local/bin/msedgedriver', options=options)
 driver.get('https://www.google.com')
 
 print(driver.title)  # Imprimir el título de la página para verificar que se abrió correctamente
